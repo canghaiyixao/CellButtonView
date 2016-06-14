b0de1bad7513561cd065c5babb61f8a7f6d56444
835756267@qq.com <835756267@qq.com>
2016-06-14 12:14:49 +0800

    Initial Commit

---
+++ TextLineView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextLineView
+//
+//  Created by nbs on 16/6/14.
+//  Copyright © 2016年 周军涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TextLineView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TextLineView
+//
+//  Created by nbs on 16/6/14.
+//  Copyright © 2016年 周军涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
